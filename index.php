db906879c8b1c5fc806f7969b63178eba0c46d34
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2022-01-19 16:05:59 +0100

    layout

--- index.php
+++ index.php
@@ -1,15 +1,48 @@
 <?php
 
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Google Faq</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@600;700;800&display=swap" rel="stylesheet">
 </head>
+
 <body>
-    
+
+    <header>
+        <div>
+            <div class="logo">
+                <img src="img/google.svg" alt="Google">
+                Privacy e termini
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Introduzione</a></li>
+                <li><a href="#">Norme sulla privacy</a></li>
+                <li><a href="#">Termini di servizio</a></li>
+                <li><a href="#">Tecnologie</a></li>
+                <li class="active"><a href="#">Domande frequenti</a></li>
+
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+
+        </div>
+    </main>
+
 </body>
+
 </html>
